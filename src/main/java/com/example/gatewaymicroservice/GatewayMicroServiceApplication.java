8a501fe7c806e625b118363075a1c38f8ec93bff
lachhab nacer <lachhab.nacer@gmail.com>
2023-01-26 20:51:10 +0100

    dynamic config; done exploting eureka serve that is a discorvry service

--- src/main/java/com/example/gatewaymicroservice/GatewayMicroServiceApplication.java
+++ src/main/java/com/example/gatewaymicroservice/GatewayMicroServiceApplication.java
@@ -3,6 +3,9 @@ package com.example.gatewaymicroservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +17,24 @@ public class GatewayMicroServiceApplication {
         SpringApplication.run(GatewayMicroServiceApplication.class, args);
     }
 
+    //static config
+//    @Bean
+//    public RouteLocator routesLocator(RouteLocatorBuilder routeBuilder){
+//        return routeBuilder.routes()
+//                .route(r->r.path("/costumers/**").uri("lb://COSTUMER-SERVICE"/*"http://localhost:8081/"*/))
+//                .route(r->r.path("/products/**").uri("lb://INVENTORY-SERVICE"/*"http://localhost:8082/"*/))
+//                .build();
+//    }
+
+    //config dynamic
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder routeBuilder){
-        return routeBuilder.routes()
-                .route(r->r.path("/costumers/**").uri("http://localhost:8081/"))
-                .route(r->r.path("/products/**").uri("http://localhost:8082/"))
-                .build();
+    public DiscoveryClientRouteDefinitionLocator dynamicRoutesLocator(
+            ReactiveDiscoveryClient rdc,
+            DiscoveryLocatorProperties dlp)
+    {
+        //http://localhost:8888/INVENTORY-SERVICE/products
+        //http://localhost:8888/COSTUMER-SERVICE/costumers
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
     }
 
     @Bean
