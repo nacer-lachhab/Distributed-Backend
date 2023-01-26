4bdb3950a3dfab600d276b41864f5e4f881ca6dd
lachhab nacer <lachhab.nacer@gmail.com>
2023-01-26 20:09:41 +0100

    second version: configuration on the bean routeLocator

--- src/main/java/com/example/gatewaymicroservice/GatewayMicroServiceApplication.java
+++ src/main/java/com/example/gatewaymicroservice/GatewayMicroServiceApplication.java
@@ -1,7 +1,11 @@
 package com.example.gatewaymicroservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayMicroServiceApplication {
@@ -10,4 +14,16 @@ public class GatewayMicroServiceApplication {
         SpringApplication.run(GatewayMicroServiceApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder routeBuilder){
+        return routeBuilder.routes()
+                .route(r->r.path("/costumers/**").uri("http://localhost:8081/"))
+                .route(r->r.path("/products/**").uri("http://localhost:8082/"))
+                .build();
+    }
+
+    @Bean
+    CommandLineRunner run(){
+        return args -> System.out.println("Gateway started with success...");
+    }
 }
